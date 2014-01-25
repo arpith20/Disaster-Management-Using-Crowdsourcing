7cd0c6d47b4ee7142500edf44b9baa7e308a6955
Arpith <arpith@arpith-XPS.(none)>
2014-01-25 15:54:35 +0530

    First commit

---
+++ android/dmucs/gen/com/arpith/dmucs/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.arpith.dmucs;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/dmucs/src/com/arpith/dmucs/APreference.java
@@ -0,0 +1,13 @@
+package com.arpith.dmucs;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class APreference extends PreferenceActivity {
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    addPreferencesFromResource(R.xml.advancepreference);
+	  }
+	} 
+
