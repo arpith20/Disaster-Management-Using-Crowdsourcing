9b77121ef426baa5db62cab1e1d7db2beee5f3f3
Arpith <arpith@live.com>
2014-04-05 08:25:32 +0530

    Implemented maps in Missing person report

--- android/dmucs/src/com/arpith/dmucs/MissingPersonInfo.java
+++ android/dmucs/src/com/arpith/dmucs/MissingPersonInfo.java
@@ -11,6 +11,7 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.SharedPreferences;
@@ -21,9 +22,16 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.koushikdutta.ion.Ion;
 
-public class MissingPersonInfo extends SwipeBackActivity {
+public class MissingPersonInfo extends Activity {
 	Boolean d;
 	int success;
 
@@ -34,6 +42,8 @@ public class MissingPersonInfo extends SwipeBackActivity {
 	JSONParser jsonParser = new JSONParser();
 
 	private static String url_account;
+	
+	private GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -89,6 +99,16 @@ public class MissingPersonInfo extends SwipeBackActivity {
 			loc.setText("(" + lat + ", " + lng + ")");
 			dress.setText(dr);
 			desc.setText(description);
+			
+			LatLng reportlocation = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
+			map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+					.getMap();
+			Marker ReportLocation = map.addMarker(new MarkerOptions()
+			.position(reportlocation)
+			.icon(BitmapDescriptorFactory
+					.fromResource(R.drawable.currentmarker)));
+			map.animateCamera(CameraUpdateFactory.newLatLngZoom(reportlocation, 9), 4000,
+					null);
 		}
 
 	}
@@ -144,13 +164,5 @@ public class MissingPersonInfo extends SwipeBackActivity {
 			pDialog.dismiss();
 		}
 
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		SwipeBackLayout mSwipeBackLayout;
-		mSwipeBackLayout = getSwipeBackLayout();
-		mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
 	}
 }
