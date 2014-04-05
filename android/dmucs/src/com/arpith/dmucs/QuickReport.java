9512eb8c4753ca0ec2f8e8f159c8625eb7642645
Arpith <arpith@live.com>
2014-04-05 22:06:01 +0530

    improved incident reporting module

--- android/dmucs/src/com/arpith/dmucs/QuickReport.java
+++ android/dmucs/src/com/arpith/dmucs/QuickReport.java
@@ -3,43 +3,51 @@ package com.arpith.dmucs;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import me.imid.swipebacklayout.lib.SwipeBackLayout;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class QuickReport extends Activity {
+public class QuickReport extends Activity implements OnMapClickListener{
 	
 	Button Submit_Report;
+	
+	private GoogleMap map;
+	LatLng currentLocation;
+	
+	String lat, lng, phone;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quick_report);
 		
-		
-		
-		Spinner spinner1 = (Spinner) findViewById(R.id.category_spinner);
-        List<String> list = new ArrayList<String>();
-        list.add("Earthquake");
-        list.add("Tsunami");
-        list.add("Flood");
-        list.add("Draught");
-        list.add("Fire");
-         
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>
-                     (this, R.layout.spinner_item,list);
-                      
-        dataAdapter.setDropDownViewResource
-                     (android.R.layout.simple_spinner_dropdown_item);
-                      
-        spinner1.setAdapter(dataAdapter);
+		AutoCompleteTextView incident = (AutoCompleteTextView)findViewById(R.id.autocomplete_incident);
+		String[] incidents = getResources().getStringArray(R.array.incidents);
+		ArrayAdapter<String> adapter = 
+		        new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, incidents);
+		incident.setAdapter(adapter);
 		
 		Submit_Report=(Button) findViewById(R.id.qr_submit);
 		Submit_Report.setOnClickListener(new View.OnClickListener() {
@@ -51,7 +59,45 @@ public class QuickReport extends Activity {
 			}
 		});
 		
+		SharedPreferences uname = getSharedPreferences("user", 0);
+		phone = uname.getString("name", "0");
 		
+		location();
+		
+	}
+	
+	private void location() {
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		Criteria criteria = new Criteria();
+		String provider = locationManager.getBestProvider(criteria, true);
+		Location lastKnownLocation = locationManager
+				.getLastKnownLocation(provider);
+		currentLocation = new LatLng(lastKnownLocation.getLatitude(),
+				lastKnownLocation.getLongitude());
+
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		map.setOnMapClickListener(this);
+		
+		map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 14), 2000,
+				null);
+	}
+
+	@Override
+	public void onMapClick(LatLng POINT) {
+		map.clear();
+		lng=String.valueOf(POINT.longitude);
+		lat=String.valueOf(POINT.latitude);
+
+		Marker incident = map.addMarker(new MarkerOptions()
+				.position(POINT)
+				.icon(BitmapDescriptorFactory
+						.fromResource(R.drawable.incidentmarker)));
+		
+		// moves camera to specified location
+		map.animateCamera(CameraUpdateFactory.newLatLngZoom(POINT, 15), 2000,
+				null);
 		
 	}
 }
